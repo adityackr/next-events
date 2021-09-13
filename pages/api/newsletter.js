a2fbbae4fd9335ffc8184b3f700ddf2f37a212df
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-09-13 15:52:08 +0600

    newsletter connected to mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
 
@@ -6,7 +8,16 @@ const handler = (req, res) => {
             res.status(422).json({ message: 'Invalid email address.' });
         }
 
-        console.log(userEmail);
+        const client = await MongoClient.connect(
+            'mongodb+srv://adi:g6v2z4auqmIwpLRq@cluster0.usjbg.mongodb.net/events?retryWrites=true&w=majority'
+        );
+
+        const db = client.db();
+
+        await db.collection('newsletter').insertOne({ email: userEmail });
+
+        client.close();
+
         res.status(201).json({ message: 'Signed up!' });
     }
 };
