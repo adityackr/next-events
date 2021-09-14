d47800840e8ce698f964a301f68c5e1dabba708d
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-09-14 10:38:51 +0600

    added context state

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,29 @@
 import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 import Notification from '../components/ui/Notification';
+import { NotificationContextProvider } from '../store/notification-context';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <title>NextEvents</title>
-                <meta name="description" content="NextEvents" />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
+        <NotificationContextProvider>
+            <Layout>
+                <Head>
+                    <title>NextEvents</title>
+                    <meta name="description" content="NextEvents" />
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
+                    />
+                </Head>
+                <Component {...pageProps} />
+                <Notification
+                    title="Test"
+                    message="This is a test!"
+                    status="error"
                 />
-            </Head>
-            <Component {...pageProps} />
-            <Notification
-                title="Test"
-                message="This is a test!"
-                status="error"
-            />
-        </Layout>
+            </Layout>
+        </NotificationContextProvider>
     );
 }
 
